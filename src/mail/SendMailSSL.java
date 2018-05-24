d6bf61f47fe0ab176cbdd03decf45c6a3494a7c5
Siva Cheerla <sivacheerla@live.com>
2018-05-25 00:02:05 +0530

    24/05/2018

---
+++ src/mail/SendMailSSL.java
@@ -0,0 +1,41 @@
+package mail;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMailSSL {
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("username@gmail.com", "********");
+			}
+		});
+
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("username@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("username@gmail.com"));
+			message.setSubject("A mail from java program!");
+			message.setText("Hi, This is Siva Cheerla I'm sending this mail from java program..");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
