3bb782975c46131ba11151389cac64216d0966f2
Siva Cheerla <sivacheerla@live.com>
2018-08-07 21:56:20 +0530

    07/08/2018

--- src/inheritance/Employee.java
+++ src/inheritance/Employee.java
@@ -33,3 +33,35 @@ class Manager extends Employee {
 		System.out.println(this.hra);
 	}
 }
+
+class SeniorManager extends Manager {
+	protected int da;
+
+	public SeniorManager(String name, int salary, int hra, int da) {
+		super(name, salary, hra);
+		this.da = da;
+	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println(this.da);
+	}
+}
+
+class MarketingExceutive extends Employee {
+	protected int ta;
+
+	public MarketingExceutive(String name, int salary, int ta) {
+		super(name, salary);
+		this.ta = ta;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println(this.ta);
+	}
+
+}
--- src/inheritance/TestEmployee.java
+++ src/inheritance/TestEmployee.java
@@ -6,9 +6,20 @@ public class TestEmployee {
 		// TODO Auto-generated method stub
 		Employee e = new Employee("Steve", 100000);
 		Manager m = new Manager("Bill", 80000, 3000);
+		SeniorManager sm = new SeniorManager("Premji", 50000, 10000, 5000);
+		MarketingExceutive me = new MarketingExceutive("Siva", 25000, 5000);
+
+		System.out.println("Employee: ");
 		print(e);
 		System.out.println();
+		System.out.println("Manager: ");
 		print(m);
+		System.out.println();
+		System.out.println("Senior Manager: ");
+		print(sm);
+		System.out.println();
+		System.out.println("Marketing Executive: ");
+		print(me);
 	}
 
 	public static void print(Employee e) {
