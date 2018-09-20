2fe136f827d5a56d7f91677c82a2d6bb2ad7aa79
Siva Cheerla <sivacheerla@live.com>
2018-09-20 21:57:44 +0530

    20/09/2018

--- src/database/with_mysql/DBUtil.java
+++ src/database/with_mysql/DBUtil.java
@@ -6,13 +6,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class DBUtil {
-	// note: need to download mysql-connector jar file and put it in jre/lib/ext folder
+	// note: need to download mysql-connector jar file and put it in jre/lib/ext
+	// folder
 	public static Connection getConnection() {
 		Connection con = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			// for mysql connector 5.1, Class is com.mysql.jdbc.Driver
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/python", "root", "**********");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pantry", "root", "9866850403");
 			PreparedStatement ps = con.prepareStatement("show tables");
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
---
+++ src/database/with_mysql/GetEmployee.java
@@ -0,0 +1,34 @@
+package database.with_mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class GetEmployee {
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mock", "root", "9866850403");
+			PreparedStatement stmt = con
+					.prepareStatement("select * from employee where fn = ? and ln = 'Cheerla' and city = ?");
+			stmt.setString(1, "Siva");
+			stmt.setString(2, "Vizag");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next())
+				System.out.println(rs.getString(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			con.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		getConnection();
+	}
+}
