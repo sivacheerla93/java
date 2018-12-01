e88b4d3097242dc4f5740567cd491902a0b716e1
Siva Cheerla <sivacheerla@live.com>
2018-12-01 15:46:52 +0530

    01/12/18

---
+++ src/database/with_mysql/PSTExecuteUpdate1.java
@@ -0,0 +1,49 @@
+package database.with_mysql;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class PSTExecuteUpdate1 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pantry", "root", "9866850403");
+			ps = con.prepareStatement("insert into coffee values(?,'Cafetino',?)");
+			Scanner scan = new Scanner(System.in);
+
+			do {
+				System.out.println("Coffee id:");
+				int id = scan.nextInt();
+				System.out.println("Sales:");
+				int sales = scan.nextInt();
+
+				ps.setInt(1, id);
+				ps.setInt(2, sales);
+
+				int i = ps.executeUpdate();
+				System.out.println(i + " records affected");
+
+				System.out.println("Do you want to continue: y/n");
+				String s = scan.next();
+				if (s.equals("n")) {
+					System.out.println("Bye! Bye!!");
+					break;
+				}
+			} while (true);
+			ps.close();
+			con.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
