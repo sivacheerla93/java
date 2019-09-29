cab3d87d008575481c47de6a1a007d5214267597
Siva Cheerla <sivacheerla@live.com>
2019-09-29 21:59:21 +0530

    29/09/19

--- src/lambda/PrintJavaFiles.java
+++ src/lambda/PrintJavaFiles.java
@@ -3,10 +3,11 @@ package lambda;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class PrintJavaFiles {
 	public static void main(String[] args) throws Exception {
-		Path dir = Paths.get("D:\\Education");
+		Path dir = Paths.get("f:\\classroom\\nov28");
 
 		Files.walk(dir).filter(p -> p.toString().endsWith(".java")).forEach(PrintJavaFiles::printFile);
 	}
@@ -14,7 +15,12 @@ public class PrintJavaFiles {
 	public static void printFile(Path p) {
 		System.out.printf("\n******* %s *******\n", p.toString());
 		try {
-			Files.readAllLines(p).stream().forEach(System.out::println);
+			List<String> lines = Files.readAllLines(p);
+			int lineno = 1;
+			for (String line : lines) {
+				System.out.printf("%3d : %s\n", lineno, line);
+				lineno++;
+			}
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
---
+++ src/swing/BorderLayoutDemo.java
@@ -0,0 +1,37 @@
+package swing;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class BorderLayoutDemo extends javax.swing.JFrame {
+
+	public BorderLayoutDemo() {
+		super("BorderLayout Demo");
+		// default layout manager is BorderLayout
+		Container c = getContentPane();
+		c.add(new JButton("Top Button"), BorderLayout.PAGE_START);
+		c.add(new JButton("Bottom Button"), BorderLayout.PAGE_END);
+		c.add(new JButton("Left Button"), BorderLayout.LINE_START);
+		c.add(new JButton("Right Button"), BorderLayout.LINE_END);
+		// place multiple components at the center using JPanel
+		JPanel p = new JPanel(); // default FlowLayout
+		p.add(new JLabel("Label"));
+		p.add(new JTextField(20));
+		c.add(p); // CENTER
+
+		setSize(500, 200);
+		setVisible(true);
+	}
+
+	public static void main(String args[]) {
+		new BorderLayoutDemo();
+	}
+
+	// Variables declaration - do not modify//GEN-BEGIN:variables
+	// End of variables declaration//GEN-END:variables
+
+}
